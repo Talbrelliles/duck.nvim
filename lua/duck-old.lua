4e033fca700930c5bfab2d8a1fdc5915ddda0efc
Talmage Goodman <utilitytitan@gmail.com>
2025-03-16 16:37:06 -0600

    start of rewrite

---
+++ lua/duck-old.lua
@@ -0,0 +1,123 @@
+-- local M = {}
+-- local vqd
+-- local a = require("plenary.async")
+--
+-- function M.init_duck()
+-- 	local args = { "curl", "-s", "-I" }
+-- 	local headers = get_duck_headers("x-vqd-accept: 1")
+-- 	for _, header in pairs(headers) do
+-- 		table.insert(args, "-H")
+-- 		table.insert(args, header)
+-- 	end
+-- 	table.insert(args, "https://duckduckgo.com/duckchat/v1/status")
+-- 	local result = vim.fn.system(args)
+-- 	local temp_vqd = result:match("x%-vqd%-4: ([^\r\n]+)")
+-- 	return temp_vqd
+-- end
+--
+-- function M.get_vqd()
+-- 	return vqd
+-- end
+--
+-- function M.send_message(message)
+-- 	if vqd == nil then
+-- 		vqd = M.init_duck()
+-- 	else
+-- 		print("running")
+-- 	end
+-- 	local headers = get_duck_headers("x-vqd-4:" .. vqd)
+-- 	local data = '{"messages": [{"content": "' .. message .. '", "role": "user"}],"model": "o3-mini"}'
+-- 	local args = { "curl", "-s", "-d", data }
+-- 	for _, header in pairs(headers) do
+-- 		table.insert(args, "-H")
+-- 		table.insert(args, header)
+-- 	end
+-- 	table.insert(args, "https://duckduckgo.com/duckchat/v1/chat")
+-- 	local result = vim.fn.system(args)
+-- 	local response = M.parse_json(result)
+-- end
+--
+-- function M.get_messages(data)
+-- 	local json_objects = {}
+-- 	for line in string.gmatch(data, "[^\n]+") do
+-- 		if line:sub(1, 6) == "data: " then
+-- 			local json = line:sub(7)
+-- 			table.insert(json_objects, json)
+-- 		end
+-- 	end
+-- 	return json_objects
+-- end
+--
+-- function M.parse_json(data)
+-- 	local message_str = ""
+-- 	local messages = M.get_messages(data)
+-- 	for _, message in pairs(messages) do
+-- 		local success, complete_message = pcall(vim.json.decode, message)
+-- 		if success and complete_message.message then
+-- 			message_str = message_str .. complete_message.message
+-- 		end
+-- 	end
+-- 	M.buffer_tests(message_str)
+-- end
+--
+-- function get_duck_headers(vqd_val)
+-- 	local headers = {
+-- 		"Connection: keep-alive",
+-- 		"Sec-Fetch-Site: same-origin",
+-- 		"User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0",
+-- 		"Accept-Encoding: gzip, deflate, br",
+-- 		"Cookie: dcm=1",
+-- 		"Sec-Fetch-Mode: cors",
+-- 		"Content-Type: application/json",
+-- 		"TE: trailers",
+-- 		"Sec-Fetch-Dest: empty",
+-- 		"Referer: https://duckduckgo.com/",
+-- 		"Origin: https://duckduckgo.com",
+-- 		"Cache-Control: no-store",
+-- 		"Accept-Language: en-US;q=0.7,en;q=0.3",
+-- 		"Pragma: no-cache",
+-- 		vqd_val,
+-- 		"Accept: text/event-stream",
+-- 	}
+-- 	return headers
+-- end
+--
+-- function M.curl_test()
+-- 	M.send_message()
+-- end
+--
+-- function M.buffer_tests(input)
+-- 	local lines = {}
+-- 	if type(input) == "table" then
+-- 		lines = input
+-- 	else
+-- 		for line in input:gmatch("([^\n]*)\n?") do
+-- 			table.insert(lines, line)
+-- 		end
+-- 	end
+-- 	local buf = vim.api.nvim_create_buf(false, true)
+-- 	vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+-- 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+-- 	local total_width = vim.o.columns
+-- 	local new_width = math.floor(total_width * 0.3)
+-- 	vim.cmd("vsplit")
+-- 	vim.api.nvim_set_current_buf(buf)
+-- 	local new_win_id = vim.api.nvim_get_current_win()
+-- 	vim.api.nvim_win_set_width(new_win_id, new_width)
+-- 	vim.api.nvim_win_set_option(new_win_id, "wrap", true)
+-- 	vim.api.nvim_win_set_option(new_win_id, "linebreak", true)
+-- 	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":bd<CR>", { noremap = true, silent = true })
+-- end
+--
+-- function M.setup(opts)
+-- 	-- TODO: Default keybindings
+-- 	opts = opts or {}
+-- 	vim.api.nvim_create_user_command("DuckAsk", function(call)
+-- 		M.send_message(call.args)
+-- 	end, {
+-- 		nargs = 1,
+-- 		desc = "Ask duckduckgo ai a question",
+-- 	})
+-- end
+--
+-- return M
--- lua/duck.lua
+++ lua/duck.lua
@@ -1,65 +1,107 @@
 local M = {}
-local vqd
+local Job
 
-function M.init_duck()
-	local args = { "curl", "-s", "-I" }
-	local headers = get_duck_headers("x-vqd-accept: 1")
-	for _, header in pairs(headers) do
-		table.insert(args, "-H")
-		table.insert(args, header)
+local vqd = nil
+local buffer
+
+function M.setup(opts)
+	opts = opts or {}
+	Job = require("plenary.job")
+	if not Job then
+		error("Failed to load plenary.job")
 	end
-	table.insert(args, "https://duckduckgo.com/duckchat/v1/status")
-	local result = vim.fn.system(args)
-	local temp_vqd = result:match("x%-vqd%-4: ([^\r\n]+)")
-	return temp_vqd
+	buffer = M.setup_buffer()
+	vim.api.nvim_create_user_command("DuckAsk", function(call)
+		M.send_message(call.args)
+	end, {
+		nargs = 1,
+		desc = "Ask duckduckgo ai a question",
+	})
 end
 
-function M.get_vqd()
-	return vqd
+function M.setup_buffer()
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":bd<CR>", { noremap = true, silent = true })
+	return buf
+end
+
+function M.init_duck()
+	local args = M.get_curl_args("x-vqd-accept: 1", nil, "https://duckduckgo.com/duckchat/v1/status")
+	Job:new({
+		command = "curl",
+		args = args,
+		cwd = ".",
+		on_stdout = function(j, return_val)
+			if return_val == 0 then
+				vqd = j:result():match("x%-vqd%-4: ([^\r\n]+)")
+			else
+				print("failure in curl")
+				print(return_val)
+			end
+		end,
+		on_stderr = function(j, return_val)
+			print(j:result())
+		end,
+	}):start()
 end
 
 function M.send_message(message)
-	if vqd == nil then
-		vqd = M.init_duck()
-	else
-		print("running")
+	if vqd then
+		local vqd_header = "x-vqd-4:" .. vqd
+		local data = '{"messages": [{"content": "' .. message .. '", "role": "user"}],"model": "o3-mini"}'
+		local args = M.get_curl_args(vqd_header, data, "https://duckduckgo.com/duckchat/v1/chat")
+		Job:new({
+			command = "curl",
+			args = args,
+			cwd = ".",
+			on_stdout = function(j, return_val)
+				if return_val == 0 then
+					M.write_message_to_buffer(j:result())
+				end
+			end,
+		}):start()
 	end
-	local headers = get_duck_headers("x-vqd-4:" .. vqd)
-	local data = '{"messages": [{"content": "' .. message .. '", "role": "user"}],"model": "o3-mini"}'
-	local args = { "curl", "-s", "-d", data }
-	for _, header in pairs(headers) do
-		table.insert(args, "-H")
-		table.insert(args, header)
-	end
-	table.insert(args, "https://duckduckgo.com/duckchat/v1/chat")
-	local result = vim.fn.system(args)
-	local response = M.parse_json(result)
+	print("No VQD setting it....")
+	M.init_duck()
 end
 
-function M.get_messages(data)
-	local json_objects = {}
-	for line in string.gmatch(data, "[^\n]+") do
+function M.get_message(message)
+	local message_objs = {}
+	local complete_message = ""
+	for line in string.gmatch(message, "[^\n]+") do
 		if line:sub(1, 6) == "data: " then
 			local json = line:sub(7)
-			table.insert(json_objects, json)
+			table.insert(message_objs, json)
 		end
 	end
-	return json_objects
-end
-
-function M.parse_json(data)
-	local message_str = ""
-	local messages = M.get_messages(data)
-	for _, message in pairs(messages) do
-		local success, complete_message = pcall(vim.json.decode, message)
-		if success and complete_message.message then
-			message_str = message_str .. complete_message.message
+	for _, message_part_obj in pairs(message_objs) do
+		local success, message_part = pcall(vim.json.decode, message_part_obj)
+		if success and message_part.message then
+			complete_message = complete_message .. message_part.message
 		end
 	end
-	M.buffer_tests(message_str)
+	return complete_message
 end
 
-function get_duck_headers(vqd_val)
+function M.write_message_to_buffer(raw_message)
+	local message = M.get_message(raw_message)
+	local lines = {}
+	for line in message:gmatch("([^\n]*)\n?") do
+		table.insert(lines, line)
+	end
+	vim.api.nvim_buf_set_lines(buffer, 0, -1, false, lines)
+	local total_width = vim.o.columns
+	local new_width = math.floor(total_width * 0.3)
+	vim.cmd("vsplit")
+	vim.api.nvim_set_current_buf(buffer)
+	local new_win_id = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_width(new_win_id, new_width)
+	vim.api.nvim_win_set_option(new_win_id, "wrap", true)
+	vim.api.nvim_win_set_option(new_win_id, "linebreak", true)
+end
+
+function M.get_duck_headers(vqd_header)
 	local headers = {
 		"Connection: keep-alive",
 		"Sec-Fetch-Site: same-origin",
@@ -75,48 +117,25 @@ function get_duck_headers(vqd_val)
 		"Cache-Control: no-store",
 		"Accept-Language: en-US;q=0.7,en;q=0.3",
 		"Pragma: no-cache",
-		vqd_val,
+		vqd_header,
 		"Accept: text/event-stream",
 	}
 	return headers
 end
 
-function M.curl_test()
-	M.send_message()
-end
-
-function M.buffer_tests(input)
-	local lines = {}
-	if type(input) == "table" then
-		lines = input
-	else
-		for line in input:gmatch("([^\n]*)\n?") do
-			table.insert(lines, line)
-		end
+function M.get_curl_args(vqd_header, data, endpoint)
+	local args = { "-s", "-I" }
+	if data then
+		table.insert(args, "-d")
+		table.insert(args, data)
 	end
-	local buf = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
-	local total_width = vim.o.columns
-	local new_width = math.floor(total_width * 0.3)
-	vim.cmd("vsplit")
-	vim.api.nvim_set_current_buf(buf)
-	local new_win_id = vim.api.nvim_get_current_win()
-	vim.api.nvim_win_set_width(new_win_id, new_width)
-	vim.api.nvim_win_set_option(new_win_id, "wrap", true)
-	vim.api.nvim_win_set_option(new_win_id, "linebreak", true)
-	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":bd<CR>", { noremap = true, silent = true })
-end
-
-function M.setup(opts)
-	-- TODO: Default keybindings
-	opts = opts or {}
-	vim.api.nvim_create_user_command("DuckAsk", function(call)
-		M.send_message(call.args)
-	end, {
-		nargs = 1,
-		desc = "Ask duckduckgo ai a question",
-	})
+	local headers = M.get_duck_headers(vqd_header)
+	for _, header in pairs(headers) do
+		table.insert(args, "-H")
+		table.insert(args, header)
+	end
+	table.insert(args, endpoint)
+	return args
 end
 
 return M
